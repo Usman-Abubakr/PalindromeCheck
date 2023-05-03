491797fc50393d6e84a96ac8750820d676489353
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-03 16:10:55 +0100

    first commit

--- src/main/java/com/sparta/mua/Main.java
+++ src/main/java/com/sparta/mua/Main.java
@@ -1,7 +1,19 @@
 package com.sparta.mua;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Please enter a word: ");
+        String word = scanner.next();
+
+
+
+
+
     }
 }
