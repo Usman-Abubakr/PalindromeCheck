12af2ea1b804f0060d8199869865abca9bcbd1ad
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-03 16:43:25 +0100

    Added tests

--- src/test/java/com/sparta/mua/MainTests.java
+++ src/test/java/com/sparta/mua/MainTests.java
@@ -1,7 +1,22 @@
 package com.sparta.mua;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class MainTests {
 
+    @Test
+    @DisplayName("check that racecar return true")
+    void checkGreaterThan3() {
+        Assertions.assertEquals(true, Main.isValidWord("racecar"));
+    }
 
+    @Test
+    @DisplayName("check that racecar return false")
+    void checkLessThan3() {
+        Assertions.assertEquals(false, Main.isValidWord("ab"));
+    }
 
 }
