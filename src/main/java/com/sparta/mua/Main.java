fb56b4afd5076d4eb274a0be2584515848d122c6
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-03 17:32:35 +0100

    Palindrome task 1 complete

--- src/main/java/com/sparta/mua/Main.java
+++ src/main/java/com/sparta/mua/Main.java
@@ -6,36 +6,36 @@ public class Main {
 
 
     public static void main(String[] args) {
-        getWord();
-    }
 
-
-
-
-    public static void getWord(){
         Scanner scanner = new Scanner(System.in);
+        StringBuilder word = new StringBuilder();
+        StringBuilder reversedWord = new StringBuilder();
 
         System.out.println("Please enter a word: ");
-        String word = scanner.next();
+        String enteredWord = scanner.next();
 
-        if (isValidWord(word)) {
-            word = word.toLowerCase();
-            print(word);
+        if (validateWord(enteredWord)) {
+            word.append(enteredWord.toLowerCase());
+            reversedWord.append(word).reverse();
+
+            if ((word.toString()).equals(reversedWord.toString())) {
+                System.out.println(word + " is a palindrome.");
+            }
+            else {
+                System.out.println(word + " is NOT a palindrome.");
+            }
         }
     }
 
-    public static Boolean isValidWord(String word){
+
+    public static Boolean validateWord(String word){
         if (word.length() < 3) {
+            System.out.println("Not a valid word!");
             return false;
         }
         else {
+            System.out.println("Word entered: " + word);
             return true;
         }
     }
-
-    public static void print(String word){
-        System.out.println(word);
-    }
-
-
 }
--- src/test/java/com/sparta/mua/MainTests.java
+++ src/test/java/com/sparta/mua/MainTests.java
@@ -3,20 +3,19 @@ package com.sparta.mua;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class MainTests {
 
     @Test
     @DisplayName("check that racecar return true")
     void checkGreaterThan3() {
-        Assertions.assertEquals(true, Main.isValidWord("racecar"));
+        Assertions.assertEquals(true, Main.validateWord("racecar"));
     }
 
     @Test
     @DisplayName("check that racecar return false")
     void checkLessThan3() {
-        Assertions.assertEquals(false, Main.isValidWord("ab"));
+        Assertions.assertEquals(false, Main.validateWord("ab"));
     }
 
 }
