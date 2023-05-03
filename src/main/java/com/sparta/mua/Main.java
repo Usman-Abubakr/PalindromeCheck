757ce0b9237aa315e6383c7eeb1083773f025c1f
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-03 16:43:16 +0100

    Added word verification

--- src/main/java/com/sparta/mua/Main.java
+++ src/main/java/com/sparta/mua/Main.java
@@ -3,17 +3,39 @@ package com.sparta.mua;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
+        getWord();
+    }
 
+
+
+
+    public static void getWord(){
         Scanner scanner = new Scanner(System.in);
-
 
         System.out.println("Please enter a word: ");
         String word = scanner.next();
 
-
-
-
-
+        if (isValidWord(word)) {
+            word = word.toLowerCase();
+            print(word);
+        }
     }
+
+    public static Boolean isValidWord(String word){
+        if (word.length() < 3) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public static void print(String word){
+        System.out.println(word);
+    }
+
+
 }
